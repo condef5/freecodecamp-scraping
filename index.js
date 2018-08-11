6b99a394fa94300fcefdbfd63c09a302dd018d94
condef5 <davis.con.fab@gmail.com>
2018-08-11 00:20:11 -0500

    Fetch users from github

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const express = require('express');
+const fetch = require('node-fetch');
+
 const app = express();
 const port = process.env.PORT || '3000';
-const { users } = JSON.parse(fs.readFileSync('users.json'));
+const urlUsers = 'https://raw.githubusercontent.com/condef5/freecodecamp-scraping/master/users.json';
+
+async function fetchAsync() {
+  let response = await fetch(urlUsers);
+  let data = await response.json();
+  return data;
+}
 
 app.use(express.static('public'));
 
@@ -13,40 +21,47 @@ app.get('/', (req, res) => {
 
 app.get('/data', async (req, res, next) => {
   let response = [];
-  try {
-    puppeteer.launch({
-      headless: true,
-      args: ['--no-sandbox',
-        '--disable-setuid-sandbox',
-      ]
-    }).then(async browser => {
-      const promises = []
-      for (let i = 0; i < users.length; i++) {
-        promises.push(browser.newPage().then(async page => {
-          await page.goto(`https://www.freecodecamp.org/${users[i]}`);
-          await page.waitFor(1200);
-          const result = await page.evaluate(() => {
-            let username = document.querySelector('.username');
-            let name = document.querySelector('.name');
-            let points = document.querySelector('.points');
-            return {
-              "username": username ? username.innerText.replace("@", "") : "",
-              "points": points ? parseInt(points.innerText.replace(" points", "")) : "",
-              "name": name ? name.innerText : ""
-            }
+  fetchAsync()
+    .then(data => {
+      const users = data.users;
+      console.log(users)
+      console.log(2)
+      try {
+        puppeteer.launch({
+          headless: true,
+          args: ['--no-sandbox',
+            '--disable-setuid-sandbox',
+          ]
+        }).then(async browser => {
+          const promises = []
+          for (let i = 0; i < users.length; i++) {
+            promises.push(browser.newPage().then(async page => {
+              await page.goto(`https://www.freecodecamp.org/${users[i]}`);
+              await page.waitFor(1200);
+              const result = await page.evaluate(() => {
+                let username = document.querySelector('.username');
+                let name = document.querySelector('.name');
+                let points = document.querySelector('.points');
+                return {
+                  "username": username ? username.innerText.replace("@", "") : "",
+                  "points": points ? parseInt(points.innerText.replace(" points", "")) : "",
+                  "name": name ? name.innerText : ""
+                }
+              });
+              return result;
+            }))
+          }
+          await Promise.all(promises).then(values => {
+            response = values;
           });
-          return result;
-        }))
+          await browser.close()
+          res.json(response);
+        })
+      } catch (e) {
+        next(e)
       }
-      await Promise.all(promises).then(values => {
-        response = values;
-      });
-      await browser.close()
-      res.json(response);
     })
-  } catch (e) {
-    next(e)
-  }
+    .catch(reason => console.log(reason.message))
 })
 
 app.listen(3000, function () {
