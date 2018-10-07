14c9e348a70c5c791e7456f1bc6d5eed721ead37
Rasmus Falk <rf222fu@student.lnu.se>
2018-10-07 08:28:10 +0200

    Show users as grid

--- public/js/main.js
+++ public/js/main.js
@@ -3,20 +3,14 @@ fetch("/data")
   .then(response => response.json())
   .then(data => {
     data = data.sort((a, b) => b.points - a.points);
-    let html = `
-      <div class="list__header">
-        <div>User</div>
-        <div>Points</div>
-      </div>
-    `
+    let html = ''
+    
     data.forEach((user, index) => {
       html += `
         <div class="list__user">
-          <div class="username">
-            <span>${index + 1}. ${user.name.toLowerCase()}</span>
-            <a href="https://www.freecodecamp.org/${user.username}" target="_blank">@${user.username}</a>
-          </div>
-          <div class="points">${user.points}</div>
+          <h3 class="username">${user.name.toLowerCase()}</h3>
+          <h3 class="points">${user.points}</h3>
+          <a href="https://www.freecodecamp.org/${user.username}" target="_blank">@${user.username}</a>
         </div>
         `
     });
